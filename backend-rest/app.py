f99311e010804a76c4e06ca0471e6ac0bddcb3bb
Victor Lockwood <vlockwoo@oswego.edu>
2022-10-22 17:07:00 -0400

    Added README.md with instructions on getting the backend REST app running locally

--- backend-rest/app.py
+++ backend-rest/app.py
@@ -12,7 +12,7 @@ import ResponseClasses
 app = Flask(__name__)
 
 
-# http://127.0.0.1:5000/
+# http://localhost:5000/mapdata
 
 # Get the current map data
 # Note: Currently has dummy data
@@ -39,5 +39,9 @@ def disp(num):
 
 # driver function
 if __name__ == '__main__':
+    # This is important for Docker
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
