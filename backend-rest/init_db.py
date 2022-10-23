f5a526c83198d2f25503e06d4467ca22c3eaebd7
Victor Lockwood <vlockwoo@oswego.edu>
2022-10-23 13:51:28 -0400

    Created Postgres connection code, updated server_backup.sql

---
+++ backend-rest/init_db.py
@@ -0,0 +1,35 @@
+# Followed code example from here:
+# https://www.digitalocean.com/community/tutorials/how-to-use-a-postgresql-database-in-a-flask-application
+
+import os
+import psycopg2
+
+os.environ['DB_PASSWORD'] = ""
+password = os.environ['DB_PASSWORD']
+
+os.environ['DB_USERNAME'] = "flaskuser"
+username = os.environ['DB_USERNAME']
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="RobotRadarAlpha",
+    user=username,
+    password=password)
+
+cur = conn.cursor()
+
+cur.execute('INSERT INTO "Map" DEFAULT VALUES returning "Id";')
+
+#cur.execute('INSERT INTO books (title, author, pages_num, review)'
+#            'VALUES (%s, %s, %s, %s)',
+#            ('A Tale of Two Cities',
+#             'Charles Dickens',
+#             489,
+#             'A great classic!')
+#            )
+
+
+conn.commit()
+
+cur.close()
+conn.close()
