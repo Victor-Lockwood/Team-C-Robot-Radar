e4c8d57075170feecc0bfcc8ab35814ad4303943
Victor Lockwood <vlockwoo@oswego.edu>
2022-11-25 11:21:22 -0500

    Updated movement code

--- robot-notes/pi_Files/total-Flask-Endpoint(Copy).py
+++ robot-notes/pi_Files/total-Flask-Endpoint(Copy).py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template, Response, request, send_file, jsonify, request
+from flask import Flask, render_template, Response, request, send_file, jsonify, request
 import requests
 import gopigo
 import time
@@ -6,7 +6,6 @@ from picamera import PiCamera
 from time import sleep
 from camera_pi import Camera
 from PIL import Image
-from flask_session import Session
 import easygopigo
 import zipfile
 import io
@@ -14,10 +13,6 @@ import pathlib
 
 
 app = Flask(__name__)
-
-SESSION_TYPE = 'filesystem'
-app.config.from_object(__name__)
-Session(app)
 
 def __get_offset():
     return 5
@@ -35,17 +30,20 @@ speedOffset=8#this is necessary to know what coordinate to change
 def determineLocationChange(orientationChange = 'F', command = 'N'):
     global orientation
     global location
+    print("Orientation change: ", orientationChange)
+
     if orientationChange == 'T':
         print("you want to change the orientation")
         if command == 'R':
             if orientation == 'E':
-                orientation =='S'
+                orientation = 'S'
             elif orientation == 'N':
                 orientation = 'E'
             elif orientation == 'W':
                 orientation = 'N'
             elif orientation == 'S':
                 orientation = 'W'
+                
         elif command == 'L':
             if orientation == 'E':
                 orientation = 'N'
@@ -66,37 +64,22 @@ def determineLocationChange(orientationChange = 'F', command = 'N'):
         pass
     elif command == 'F':
         if orientation == 'E':
-            location = [round(location[0]+ 0.45, 2), round(location[1],2)]
+            location = [round(location[0] + 0.50, 2), round(location[1],2)]
         if orientation == 'N':
-            location = [round(location[0],2), round(location[1] +0.45,2)]
+            location = [round(location[0],2), round(location[1] + 0.50,2)]
         if orientation == 'W':
-            location = [round(location[0]-0.45, 2), round(location[1],2)]
+            location = [round(location[0]-0.50, 2), round(location[1],2)]
         if orientation == 'S':
             location = [round(location[0],2), round(location[1]- 0.45,2)]
     elif command == 'B':
         if orientation == 'E':
-            location = [round(location[0]-0.45, 2), round(location[1],2)]
+            location = [round(location[0]-0.50, 2), round(location[1],2)]
         if orientation == 'N':
-            location = [round(location[0],2), round(location[1]- 0.45,2)]
+            location = [round(location[0],2), round(location[1]- 0.50,2)]
         if orientation == 'W':
-            location = [round(location[0]+ 0.45,2), round(location[1],2)]
+            location = [round(location[0]+ 0.50,2), round(location[1],2)]
         if orientation == 'S':
-            location = [round(location[0],2), round(location[1] +0.45,2)]
-        
-            
-        
-            
-    '''
-    if changeLocation == 'Y':
-        if orientation == 'E':
-            location = [location[0]+ 0.01, location[1]]
-        elif orientation == 'S':
-            location = [location[0], location[1]- 0.01]
-        elif orientation == 'W':
-            location = [location[0]-0.01, location[1]]
-        else:
-            location = [location[0], location[1] +0.01]
-    '''
+            location = [round(location[0],2), round(location[1] +0.50,2)]
             
 
 @app.route('/')
@@ -122,13 +105,25 @@ def forward():
         gopigo.set_right_speed(right_speed)
         
         print("Forward!")
-        gopigo.fwd(50)#)+ speedOffset)  # Send the GoPiGo Forward
-        sleep(2.5)
+        gopigo.fwd(50)#)+ speedOffset)	# Send the GoPiGo Forward
+        
+        # Checks the motor status to make sure it's finished moving
+        while True:
+            enc_stat = gopigo.read_enc_status()
+            # print ("Current encoder status: ", enc_stat)
+            if enc_stat == 0:
+                break
+            # print("Moving forward")
+            time.sleep(0.5)
+        
+        sleep(0.5)
         determineLocationChange('F', 'F')
         #sleep(2.5)
-        print("location: ", str(location), ", orientation: ", str(orientation))
-        #gopigo.stop()  # the stop the GoPiGo
         data = my_ultrasonic.read()
+        #easygopigo.UltraSonicSensor()
+        print("location: ", str(location), ", orientation: ", str(orientation), ", radar: ", str(data))
+        #gopigo.stop()	# the stop the GoPiGo
+        
         #print("data: ", str(data))
        
         #data = my_ultrasonic.read()
@@ -152,34 +147,41 @@ def forward():
 @app.route('/backward')
 def backward():
         print("Backward!")
-        gopigo.bwd(50)  # Send the GoPiGo Backward
+        gopigo.bwd(50)	# Send the GoPiGo Backward
         #sleep(2.5)
+        
+        while True:
+            enc_stat = gopigo.read_enc_status()
+            # print ("Current encoder status: ", enc_stat)
+            if enc_stat == 0:
+                break
+            # print("Moving")
+            time.sleep(0.5)
+        
         determineLocationChange('F', 'B')
-        print("location: ", str(location), ", orientation: ", str(orientation))
-        #gopigo.stop()  # and then stop the GoPiGo.
+        time.sleep(0.5)
         data = my_ultrasonic.read()
+        print("location: ", str(location), ", orientation: ", str(orientation), ", radar: ", str(data))
         return jsonify(location = location , orientation = orientation, radar = data)        
 
 @app.route('/left')
 def left():
         print("Left!")
         gopigo.turn_left_wait_for_completion(100)
-        #sleep(2.5)
+        sleep(0.5)
         determineLocationChange('T', 'L')
-        print("location: ", str(location), ", orientation: ", str(orientation))
-        #gopigo.stop()
         data = my_ultrasonic.read()
+        print("location: ", str(location), ", orientation: ", str(orientation), ", radar: ", str(data))
         return jsonify(location= location , orientation = orientation, radar = data)
 
 @app.route('/right')
 def right():
         print("Right!")
         gopigo.turn_right_wait_for_completion(100)
-        #sleep(2.5)
+        sleep(0.5)
         determineLocationChange('T', 'R')
-        print("location: ", str(location), ", orientation: ", str(orientation))
-        #gopigo.stop()
         data = my_ultrasonic.read()
+        print("location: ", str(location), ", orientation: ", str(orientation), ", radar: ", str(data))
         return jsonify(location= location , orientation = orientation, radar = data)
     
 @app.route('/camera', methods=['GET','POST'])
@@ -251,21 +253,21 @@ def takePicture():
 
 @app.route('/dance')
 def dance():
-    print("Dance!")
-    for each in range(0,5):
-        gopigo.right()
-        time.sleep(0.25)
-        gopigo.left()
-        time.sleep(0.25)
-        gopigo.bwd()
-        time.sleep(0.25)
-    gopigo.stop()
-    return 'Dance!'
+	print("Dance!")
+	for each in range(0,5):
+		gopigo.right()
+		time.sleep(0.25)
+		gopigo.left()
+		time.sleep(0.25)
+		gopigo.bwd()
+		time.sleep(0.25)
+	gopigo.stop()
+	return 'Dance!'
 
 @app.route('/coffee')
 def coffee():
-    print("Coffee!")
-    return 'coffee!'
+	print("Coffee!")
+	return 'coffee!'
 
 @app.route('/radar')
 def radar():
@@ -287,13 +289,13 @@ def autonomous(stk):
     lst = stk.split(",")
     for i in  lst:
         if(i == 'f'):
-            gopigo.fwd(100)
+            gopigo.fwd(50)
             determineLocationChange('F', 'F')
             sleep(2.5)
             #gopigo.stop()
             #time.sleep(.25)
         if(i == 'b'):
-            gopigo.bwd(100)
+            gopigo.bwd(50)
             determineLocationChange('F', 'B')
             sleep(2.5)
             #gopigo.stop()
@@ -321,19 +323,19 @@ def autonomous(stk):
 def moveServo(inp):
         global servo_pos
         if inp=='a':
-            servo_pos=servo_pos+10  # If input is 'a' move the servo forward 10 degrees.
+            servo_pos=servo_pos+10	# If input is 'a' move the servo forward 10 degrees.
         elif inp=='d':
-            servo_pos=servo_pos-10  # If the input is 'd' move the servo backward by 10 degrees.
+            servo_pos=servo_pos-10	# If the input is 'd' move the servo backward by 10 degrees.
         elif inp=='s':
-            servo_pos=90            
-        
-    #Get the servo angles back to the normal 0 to 180 degree range
+            servo_pos=90			
+		
+	#Get the servo angles back to the normal 0 to 180 degree range
         if servo_pos>180:
             servo_pos=180
         if servo_pos<0:
             servo_pos=0
-        
-        gopigo.servo(servo_pos)     # This function updates the servo with the latest positon.  Move the servo.
+		
+        gopigo.servo(servo_pos)		# This function updates the servo with the latest positon.  Move the servo.
         time.sleep(.1)
         return 'servo position: ' + str(servo_pos)
     
@@ -362,6 +364,6 @@ def logMoxie():
     return str(data)
 
 
-    
+	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)#9871)#debug = False, host = '0.0.0.0')
+	app.run(host='0.0.0.0', port=5000)#9871)#debug = False, host = '0.0.0.0')
